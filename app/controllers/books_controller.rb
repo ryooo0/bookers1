8d4870787abee1a90d7e14af6dbe4c660cac8d91
ryooookada <btbl70120@gmail.com>
2021-10-16 06:57:44 +0000

    [Finish]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,18 @@
 class BooksController < ApplicationController
 
   def index
-    @book = Book.all
+    @book = Book.new
+    @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to post_path(@book.id)
+      redirect_to book_path(@book.id)
     else
-      redirect_to :books
+      @books = Book.all
+      render :index
     end
   end
 
@@ -26,7 +28,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to post_path(@book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
